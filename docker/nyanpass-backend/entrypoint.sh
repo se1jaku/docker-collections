8ae90eb64adc28945536003d9ab330e41ee5bc90
seijaku <178427212+se1jaku@users.noreply.github.com>
2025-12-11 23:47:41 +0800

    docker: update nyanpass-backend

--- docker/nyanpass-backend/entrypoint.sh
+++ docker/nyanpass-backend/entrypoint.sh
@@ -7,7 +7,7 @@ WORK_DIR="/opt/backend"
 GAI_CONF_FILE="/etc/gai.conf"
 BIN="rel_backend"
 
-NYAN_CONF_FILE="${NYAN_CONF_FILE:-/etc/nyanpass/config.yml}"
+NYAN_CONF_DIR="${NYAN_CONF_DIR:-/etc/nyanpass}"
 NYAN_SERVE_DIR="${NYAN_SERVE_DIR:-/srv/public}"
 NYAN_SQLITE_DATA_DIR="${NYAN_SQLITE_DATA_DIR:-/var/lib/nyanpass}"
 NYAN_SERVE_INDEX_FILE="${NYAN_SERVE_DIR}/index.html"
@@ -23,12 +23,17 @@ if [ ! -f "${FIRST_RUN}" ]; then
         echo "[entrypoint] gai.conf is readonly, skipped"
     fi
 
-    # nyan configuration file
+    # nyan data directory
     if [ ! -d "${NYAN_SQLITE_DATA_DIR}" ]; then
+        echo "[entrypoint] Preparing nyanpass data directory at ${NYAN_SQLITE_DATA_DIR}"
         mkdir -p "${NYAN_SQLITE_DATA_DIR}"
     fi
-    if [ ! -f "${NYAN_CONF_FILE}" ]; then
-        cat << EOF > "${NYAN_CONF_FILE}"
+
+    # nyan configuration directory
+    if [ ! -f "${NYAN_CONF_DIR}/config.yml" ]; then
+        echo "[entrypoint] Preparing nyanpass configuration file at ${NYAN_CONF_DIR}"
+        mkdir -p "${NYAN_CONF_DIR}"
+        cat << EOF > "${NYAN_CONF_DIR}/config.yml"
 database-path: sqlite3://${NYAN_SQLITE_DATA_DIR}/data.db
 disable-queue: false
 listen: 0.0.0.0:18888
@@ -36,11 +41,11 @@ html-path: ${NYAN_SERVE_DIR}
 disable-gzip: false
 EOF
     fi
+
     # nyan serve directory
-    if [ ! -d "${NYAN_SERVE_DIR}" ]; then
-        mkdir -p "${NYAN_SERVE_DIR}"
-    fi
     if [ ! -f "${NYAN_SERVE_INDEX_FILE}" ]; then
+        echo "[entrypoint] Preparing nyanpass static assets at ${NYAN_SERVE_DIR}"
+        mkdir -p "${NYAN_SERVE_DIR}"
         cp -r "${WORK_DIR}/public/." "${NYAN_SERVE_DIR}/"
     fi
 

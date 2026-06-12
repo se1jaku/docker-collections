832aa5b7a0094dc34dc6ffa87054c0349be2e075
seijaku <178427212+se1jaku@users.noreply.github.com>
2026-06-13 07:36:13 +0800

    docker.microbox: update

---
+++ docker/microbox/bin/entrypoint.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# first run
+FIRST_RUN="/root/.firstrun"
+RESOURCE_DIR="/res"
+CRON_ROOT_FILE="/etc/crontabs/root"
+SINGBOX_CONFIG_FILE="${SINGBOX_CONFIG_DIR}/config.json"
+
+
+echo "[entrypoint] initializing ..."
+
+if [ ! -f /etc/timezone ] || ! grep -qxF "${TZ}" /etc/timezone; then
+    echo "[entrypoint] updating timezone to ${TZ}..."
+    echo "${TZ}" > /etc/timezone
+    ln -snf "/usr/share/zoneinfo/${TZ}" /etc/localtime
+fi
+
+# check env
+if [ -z "${SINGBOX_REMOTE_URL:-}" ]; then
+    echo "SINGBOX_REMOTE_URL not set, check it manually" 2>&1
+    exit 1
+fi
+
+# check sing-box configuration file
+if [ ! -f "${SINGBOX_CONFIG_FILE}" ]; then
+    resource-manager.py ${RESOURCE_DIR} sync
+else
+    if ! output=$(sing-box -C "${SINGBOX_CONFIG_DIR}" check 2>&1); then
+        echo "[entrypoint] sing-box configuration validation failed:" >&2
+        printf '%s\n' "$output" >&2
+        exit 1
+    fi
+fi
+
+if [ ! -f "${FIRST_RUN}" ]; then
+
+    # cron resource manager self-update at 0 2 * * *
+    echo -e "0\t2\t*\t*\t*\tresource-manager.py self-update" >> $CRON_ROOT_FILE
+    # cron resource manager sync at 0 4 * * *
+    echo -e "0\t4\t*\t*\t*\tresource-manager.py ${RESOURCE_DIR} sync && supervisorctl restart sing-box" >> $CRON_ROOT_FILE
+
+    # touch first run file
+    touch ${FIRST_RUN}
+fi
+
+exec "$@"

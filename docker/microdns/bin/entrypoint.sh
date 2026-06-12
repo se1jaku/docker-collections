bd413f8240be3ed007cf166879c2e5066a449228
seijaku <178427212+se1jaku@users.noreply.github.com>
2026-06-13 05:37:08 +0800

    docker.microdns: update

--- docker/microdns/bin/entrypoint.sh
+++ docker/microdns/bin/entrypoint.sh
@@ -11,20 +11,32 @@ CRON_ROOT_FILE="/etc/crontabs/root"
 
 echo "[entrypoint] initializing ..."
 
+if [ ! -f /etc/timezone ] || ! grep -qxF "${TZ}" /etc/timezone; then
+    echo "[entrypoint] updating timezone to ${TZ}..."
+    echo "${TZ}" > /etc/timezone
+    ln -snf "/usr/share/zoneinfo/${TZ}" /etc/localtime
+fi
+
 if [ ! -f "${FIRST_RUN}" ]; then
     # when any kind of age secret is set
     if [ -n "${AGE_PASSPHRASE:-}" ] || [ -n "${AGE_SECRET_KEY:-}" ]; then
         echo "[entrypoint] age secret found!"
 
         # resource manager
-        echo "[entrypoint] rendering resources at ${RESOURCE_DIR} ..."
-        resource-manager.py ${RESOURCE_DIR} render
+        if [ -f "${RESOURCE_DIR}/.meta.json" ]; then
+            echo "[entrypoint] rendering resources at ${RESOURCE_DIR} ..."
+            resource-manager.py ${RESOURCE_DIR} render
+        fi
 
-        echo "[entrypoint] rendering resources at ${ENCRYPTION_DIR} ..."
-        resource-manager.py ${ENCRYPTION_DIR} render
+        if [ -f "${ENCRYPTION_DIR}/.meta.json" ]; then
+            echo "[entrypoint] rendering resources at ${ENCRYPTION_DIR} ..."
+            resource-manager.py ${ENCRYPTION_DIR} render
+        fi
 
+        # cron resource manager sync at 55 3 * * *
+        echo -e "55\t3\t*\t*\t*\tresource-manager.py ${RESOURCE_DIR} sync" >> $CRON_ROOT_FILE
         # cron resource manager sync at 0 4 * * *
-        echo -e "0\t4\t*\t*\t*\tresource-manager.py ${RESOURCE_DIR} sync && supervisorctl restart smartdns" >> $CRON_ROOT_FILE
+        echo -e "0\t4\t*\t*\t*\tresource-manager.py ${ENCRYPTION_DIR} sync && supervisorctl restart smartdns" >> $CRON_ROOT_FILE
     fi
 
     # touch first run file
